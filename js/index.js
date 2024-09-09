9946f8b4f9f9390848419fdd45bd8908c19fd5e8
Parth Sinha <121729564+Parthsinha20@users.noreply.github.com>
2024-09-09 23:57:03 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// typed animation 
+var typing=new Typed(".typed",{
+    strings:["web developers...","web designer...","software developers..."],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+})
+
+// button open about html
+function moreopen(){
+    window.open("about.html ","_parent")
+}
+
+
+
